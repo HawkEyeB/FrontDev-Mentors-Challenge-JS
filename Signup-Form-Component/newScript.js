051dc3f91889e551cb2cf7b3a432a6ebb0c06be7
Pedro Ingro <56962442+HawkEyeB@users.noreply.github.com>
2022-11-04 22:13:28 -0300

    Implementação da checagem do email
    Implementei uma checagem do login para mesmo se a pessoa preencher todos os campos, se o email não estiver configurado corretamente, não passará

--- Signup-Form-Component/newScript.js
+++ Signup-Form-Component/newScript.js
@@ -1,15 +1,20 @@
 "use strict";
 
 const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName").value;
-const email = document.getElementById("emailAdress").value;
-const password = document.getElementById("password").value;
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("emailAdress");
+const password = document.getElementById("password");
 const btnPromo = document.querySelector(".btn-promo");
 const btnClaim = document.querySelector(".btn-claim");
 const allForms = document.querySelectorAll(".a");
 const allError = document.querySelectorAll(".error-img");
 const errorText = document.querySelectorAll(".error-texts");
 
+let validat;
+let formValue = [];
+
+//Função para mudar a Cor dos butões
+//Function to change the color of the buttons
 function changeOpacity(x) {
   if (x.classList.contains("colorHover")) {
     btnPromo.classList.remove("colorHover");
@@ -26,42 +31,76 @@ function colorHover(x) {
   }
 }
 
+// Função para remover as classes Hidden da imagem de erro e o texto de erro
+// Function to remove the class hidden of the Error image and the error text
 function removeClass(errorImg, errorText, index) {
   errorImg[index].classList.remove("hidden");
   errorText[index].classList.remove("hidden");
 }
 
+// Função para adicionar as classes Hidden da imagem de erro e o texto de erro
+// Function to add the class hidden of the Error image and the error text
 function addClass(errorImg, errorText, index) {
   errorImg[index].classList.add("hidden");
   errorText[index].classList.add("hidden");
 }
 
-function addTextMessage(formValue) {
+function validateEmail(email) {
+  var mail = email.value
+  var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+
+  if (regx.test(mail)) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+// Função para adicionar uma mensagem se tudo foi corretamente
+// Function to add a text message if everything went correctly.
+function addTextMessage(formValue, errorImg, errorText, i, validate) {
   formValue.find(function (form) {
-    if (form !== "") {
+    if (form !== "" && validate == true) {
       console.log("hei");
-      // document.querySelector(".succed-text").classList.add("opacityIn");
+      errorImg[i].classList.add("hidden");
+      errorText[i].classList.add("hidden");
       document.querySelector(".succed-text").classList.add("opacityOut");
+    } else {
+      allError[i].classList.remove("hidden");
+      errorText[i].classList.remove("hidden");
     }
   });
 }
 
-let formValue = [];
+// Função principal
+// Principal Function
 btnClaim.addEventListener("click", function (e) {
   e.preventDefault();
   allForms.forEach(function (allForms, i) {
-    if (allForms.value == "") {
+    if (allForms.value == "" && i == 2) {
+      validateEmail(email);
       removeClass(allError, errorText, i);
       formValue.push(allForms.value);
-    } else {
+    }
+    else if (allForms.value == "") {
+      removeClass(allError, errorText, i);
+      formValue.push(allForms.value);
+    }
+    else {
       addClass(allError, errorText, i);
       formValue.push(allForms.value);
     }
+    // Uso do Arr.Find para zerar os valores do Array caso algum campo tenha sido núlo.
+    // Ele também só deixará passar se não existir nenhum valor nulo.
+    // Use Arr.Find to reset Array values ​​if any field has been null.
+    // It will also only pass if there is no null value.
     formValue.find(function (form) {
       if (form === "") {
         return (formValue = []);
       }
     });
   });
-  addTextMessage(formValue);
+  validat = validateEmail(email);
+  addTextMessage(formValue, allError, errorText, 2, validat);
 });
